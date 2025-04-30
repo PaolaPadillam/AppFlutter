5a673a08515defe9c72ffae096c06ae08b12e383
Uri <21240221@leon.tecnm.mx>
2025-04-29 19:47:53 -0600

    Uriel vista Vehiculo2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'Login.dart';
 import 'registro_usuario.dart';
+import 'bienvenida_registro_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Control Vehicular',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const LoginScreen(),
+        '/':
+            (context) =>
+                const LoginScreen(), // Puedes cambiar a BienvenidaRegistroScreen() si lo prefieres
         '/registro': (context) => const RegistroUsuarioScreen(),
+        '/bienvenida': (context) => const BienvenidaRegistroScreen(),
       },
     );
   }
